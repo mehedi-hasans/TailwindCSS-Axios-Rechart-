86dd2f0c574142950594d33b85ff66ca7b7e2396
Mehedi Hasan <shihab3676@gmail.com>
2023-09-20 12:09:05 +0600

    react-loader-spinner

--- src/compnents/Phones/Phones.jsx
+++ src/compnents/Phones/Phones.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react"
-import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts"
+import { ColorRing } from  'react-loader-spinner'
+import { Bar, BarChart, XAxis, YAxis, Tooltip } from "recharts"
 import axios from 'axios'
 export default function Phones() {
     const [phones, setPhones] = useState([])
+    const [loading, setLoading] = useState([true])
     useEffect(()=>{
         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
         .then(data => {
@@ -16,11 +18,26 @@ export default function Phones() {
             })
             console.log(phoneWithFakeData);
             setPhones(phoneWithFakeData)
+            setLoading(false)
         })
     }, [])
   return (
     <div className="text-center mt-16">
-        <h2 className="text-3xl font-bold">Phones {phones.length}</h2>
+
+            <h2 className="text-3xl font-bold">Phones {phones.length}</h2>
+        {
+            loading && <div className="mx-auto w-1/3 justify-center flex">
+            <ColorRing
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="blocks-loading"
+            wrapperStyle={{}}
+            wrapperClass="blocks-wrapper"
+            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+            />
+        </div>
+        }
         <BarChart className="mx-auto  my-9" width={1200} height={400} data={phones}>
             <Bar dataKey="price" fill='#90ee90'/>
             <XAxis dataKey='name'></XAxis>
